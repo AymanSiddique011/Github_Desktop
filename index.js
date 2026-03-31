077ac8b7659307aade088f5320f3b4f62dfbc814
Mohammad Ayman Siddique <mdayman302010@gmail.com>
2026-03-31 13:17:35 +0600

    Update JS

--- index.js
+++ index.js
@@ -3,4 +3,83 @@ var age= 21;
 var isStudent= true
 var city
 var salary= null
-console.log(age);
+//console.log(age);
+//console.log (name, salary);
+//console.log (isStudent);
+
+//Loop Strat, Loop Ending Point, Loop Increment
+        var i=0
+        for (i=0;i<100;i=i+2){
+              //console.log(i)
+        }
+
+        var x=0;
+        while(x<100){
+          //console.log(x+ " " + "for while loop")
+            x=x+1
+        }
+
+        var y=0;
+        do{
+            //console.log(y+ " " + "Do while Loop")
+            y=y+1;
+        }while(y<100)
+
+// Function Writing & Calling            
+        /*function addTwo(){
+            let name1= "Max Verstappen";
+            let name2= "Charles Leclerc";
+            console.log(name1 , name2)
+        } 
+        
+        addTwo() */
+
+    /*function addTwo(a,b){
+        let num1= a;
+        let num2= b;
+        let sum= num1+num2;
+        console.log(sum)
+    }
+    addTwo(10, 50) */
+    
+   /* function addName(a,b){
+        let name1= a;
+        let name2= b;
+        console.log(a, b)
+    }
+    addName("Max Verstappen" , "Leclerc")
+
+    function addTwo(a){
+        let num1= 100;
+        let num2= 50;
+        let num3= a;
+        let sum= num1/num2+num3;
+        console.log(sum)
+    }
+    addTwo(3) */
+    
+    // Return Function
+   /* function myFun(){
+        return 10;
+    }
+    let result=myFun()+30
+    console.log(result) */
+
+    // Arrow Function
+
+   /* myFun=()=>{
+        return 50;
+    }
+    let result=myFun()+50
+    console.log(result) */
+
+    // Imedietly Invoked Function
+
+    /* (()=>{
+        console.log("I'm Vengeance")
+    })() */
+
+    let aymanFather=()=>{
+        console.log("I'm Annonoymas Function")
+    }
+    aymanFather();    
